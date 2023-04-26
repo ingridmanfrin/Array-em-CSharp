c05e5d700d445eb1d9f0816d9dcfa89a15c9c171
ingridmanfrin <ingrid.caldeira@ITLNB109>
2023-04-26 02:51:27 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Array_em_CSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnOK = new System.Windows.Forms.Button();
+            this.ListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // BtnOK
+            // 
+            this.BtnOK.Location = new System.Drawing.Point(135, 425);
+            this.BtnOK.Name = "BtnOK";
+            this.BtnOK.Size = new System.Drawing.Size(75, 23);
+            this.BtnOK.TabIndex = 0;
+            this.BtnOK.Text = "OK";
+            this.BtnOK.UseVisualStyleBackColor = true;
+            this.BtnOK.Click += new System.EventHandler(this.BtnOK_Click);
+            // 
+            // ListBox
+            // 
+            this.ListBox.FormattingEnabled = true;
+            this.ListBox.Location = new System.Drawing.Point(34, 25);
+            this.ListBox.Name = "ListBox";
+            this.ListBox.Size = new System.Drawing.Size(302, 394);
+            this.ListBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 472);
+            this.Controls.Add(this.ListBox);
+            this.Controls.Add(this.BtnOK);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnOK;
+        private System.Windows.Forms.ListBox ListBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Array_em_CSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnOK_Click(object sender, EventArgs e)
+        {
+
+            //int[] valores;
+            //valores = new int[5]; // instancia de valores / criação de array de índice 5 (sempre começa no índice 0)
+            //valores[0] = 10;
+            //valores[1] = 20;
+            //valores[2] = 30;
+            //valores[3] = 40;
+            //valores[4] = 50;
+            //ListBox.Items.Add(valores[4] + valores[2]);
+            //arrays
+
+            //int[] valores = new int[] {10, 20, 30, 40, 50, 60, 70 };
+
+            //foreach (int valor in valores)
+            //{
+            //    ListBox.Items.Add(valor);
+            //}
+
+            //int[] valores = new int[] { 10, 20, 30, 40, 50, 60, 70 };
+
+            //for (int indice = 0; indice < 7; indice++)
+            //{
+            //    ListBox.Items.Add(valores[indice]);
+            //}
+
+            int[] valores = new int[] { 00, 500, 60, 10, 20, 30, 40, 50, 60, 10, 20, 30, 40, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190 };
+
+            for (int indice = 0; indice <valores.Length; indice++)
+            {
+                ListBox.Items.Add(valores[indice]); 
+            }
+
+        }
+    }
+}
